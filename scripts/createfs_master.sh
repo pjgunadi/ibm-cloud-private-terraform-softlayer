618595569b9cd7c2d0dab45090f28b17d3c8794c
Paulus Gunadi <paulus@sg.ibm.com>
2017-12-15 09:06:45 +0800

    disable password authentication

--- scripts/createfs_master.sh
+++ scripts/createfs_master.sh
@@ -37,3 +37,12 @@ EOL
 
 #Mount Filesystems
 mount -a
+
+#Disable password authentication on public network
+sed -i "s/^PasswordAuthentication yes$/PasswordAuthentication no/" /etc/ssh/sshd_config
+cat <<EOL | tee -a /etc/ssh/sshd_config
+
+Match Address 10.0.0.0/8,172.16.0.0/12,192.168.0.0/16
+    PasswordAuthentication yes
+EOL
+systemctl restart sshd
