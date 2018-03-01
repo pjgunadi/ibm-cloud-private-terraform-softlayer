ad63218dd16d2ec53ede17919743c93b3037bb04
Paulus Gunadi <paulus@sg.ibm.com>
2018-03-01 12:51:46 +0800

    update

--- cam/main.tf
+++ cam/main.tf
@@ -327,30 +327,30 @@ resource "ibm_compute_vm_instance" "worker" {
     ]
   }
 
-  provisioner "local-exec" {
-    when    = "destroy"
-    command = "cat > ${var.ssh_key_name} <<EOL\n${tls_private_key.ssh.private_key_pem}\nEOL"
-  }
-  provisioner "local-exec" {
-    when    = "destroy"
-    command = "chmod 600 ${var.ssh_key_name}"
-  }
+  # provisioner "local-exec" {
+  #   when    = "destroy"
+  #   command = "cat > ${var.ssh_key_name} <<EOL\n${tls_private_key.ssh.private_key_pem}\nEOL"
+  # }
+  # provisioner "local-exec" {
+  #   when    = "destroy"
+  #   command = "chmod 600 ${var.ssh_key_name}"
+  # }
   # provisioner "local-exec" {
   #   when    = "destroy"
   #   command = "scp -i ${var.ssh_key_name} ${local.ssh_options} ${path.module}/scripts/destroy/delete_worker.sh ${var.ssh_user}@${local.icp_boot_node_ip}:/tmp/"
   # }
-  provisioner "local-exec" {
-    when    = "destroy"
-    command = "ssh -i ${var.ssh_key_name} ${local.ssh_options} ${var.ssh_user}@${local.icp_boot_node_ip} \"chmod +x /tmp/delete_worker.sh; /tmp/delete_worker.sh ${var.icp_version} ${self.ipv4_address_private}\"; echo done"
-  } 
+  # provisioner "local-exec" {
+  #   when    = "destroy"
+  #   command = "ssh -i ${var.ssh_key_name} ${local.ssh_options} ${var.ssh_user}@${local.icp_boot_node_ip} \"chmod +x /tmp/delete_worker.sh; /tmp/delete_worker.sh ${var.icp_version} ${self.ipv4_address_private}\"; echo done"
+  # } 
   # provisioner "local-exec" {
   #   when    = "destroy"
   #   command = "scp -i ${var.ssh_key_name} ${local.ssh_options} ${path.module}/scripts/destroy/delete_gluster.sh ${var.ssh_user}@${local.heketi_ip}:/tmp/"
   # }
-  provisioner "local-exec" {
-    when    = "destroy"
-    command = "ssh -i ${var.ssh_key_name} ${local.ssh_options} ${var.ssh_user}@${local.heketi_ip} \"chmod +x /tmp/delete_gluster.sh; /tmp/delete_gluster.sh ${self.ipv4_address_private}\"; echo done"
-  }  
+  # provisioner "local-exec" {
+  #   when    = "destroy"
+  #   command = "ssh -i ${var.ssh_key_name} ${local.ssh_options} ${var.ssh_user}@${local.heketi_ip} \"chmod +x /tmp/delete_gluster.sh; /tmp/delete_gluster.sh ${self.ipv4_address_private}\"; echo done"
+  # }  
 }
 #Create Gluster Node
 resource "ibm_compute_vm_instance" "gluster" {
