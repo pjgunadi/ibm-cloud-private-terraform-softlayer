93b1a2a10b3b7229be7c2209b83489851a9321ae
Paulus Gunadi <paulus@sg.ibm.com>
2018-03-31 23:48:48 +0800

    update heketi cli

--- cam/scripts/destroy/delete_gluster.sh
+++ cam/scripts/destroy/delete_gluster.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 NODEIP="$1"
+HEKETI_PWD="$2"
 removeGlusterNode() {
-  NODEID=$(heketi-cli topology info | grep "$NODEIP" -B5 | sed -e 's/^[[:space:]]*//' | awk -F: -v key="Node Id" '$1==key {print $2}' | tr -d [:space:])
+  NODEID=$(heketi-cli --user admin --secret "$HEKETI_PWD" topology info | grep "$NODEIP" -B5 | sed -e 's/^[[:space:]]*//' | awk -F: -v key="Node Id" '$1==key {print $2}' | tr -d [:space:])
   if [ -n "$NODEID" ]; then
-    DEVICES=($(heketi-cli node info $NODEID | sed '1,/Devices/d' | awk -F" " '{print $1}' | awk -F: '{print $2}'))
-    heketi-cli node disable $NODEID
-    heketi-cli node remove $NODEID
+    DEVICES=($(heketi-cli --user admin --secret "$HEKETI_PWD" node info $NODEID | sed '1,/Devices/d' | awk -F" " '{print $1}' | awk -F: '{print $2}'))
+    heketi-cli --user admin --secret "$HEKETI_PWD" node disable $NODEID
+    heketi-cli --user admin --secret "$HEKETI_PWD" node remove $NODEID
     for deviceid in "${DEVICES[@]}"; do
-      heketi-cli device delete "$deviceid"
+      heketi-cli --user admin --secret "$HEKETI_PWD" device delete "$deviceid"
     done
-    heketi-cli node delete $NODEID
+    heketi-cli --user admin --secret "$HEKETI_PWD" node delete $NODEID
     sudo gluster peer detach $NODEIP
   fi
 }
